5cb658f30b092571421af566e28d2240003495d1
unknown <javieravilagalindo@gmail.com>
2024-04-24 10:40:41 -0500

    PRUEBA

---
+++ Presentacion/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Presentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.v = new System.Windows.Forms.Button();
+            this.rbMunicipio = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbDepartamento = new System.Windows.Forms.RadioButton();
+            this.Municipios = new System.Windows.Forms.ListBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnDEL = new System.Windows.Forms.Button();
+            this.btnDerecha = new System.Windows.Forms.Button();
+            this.btnIzquierda = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.Departamentos = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(330, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // v
+            // 
+            this.v.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.v.Location = new System.Drawing.Point(374, 116);
+            this.v.Name = "v";
+            this.v.Size = new System.Drawing.Size(79, 32);
+            this.v.TabIndex = 1;
+            this.v.Text = "ADD";
+            this.v.UseVisualStyleBackColor = true;
+            this.v.Click += new System.EventHandler(this.v_Click);
+            // 
+            // rbMunicipio
+            // 
+            this.rbMunicipio.AutoSize = true;
+            this.rbMunicipio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMunicipio.Location = new System.Drawing.Point(17, 19);
+            this.rbMunicipio.Name = "rbMunicipio";
+            this.rbMunicipio.Size = new System.Drawing.Size(102, 24);
+            this.rbMunicipio.TabIndex = 2;
+            this.rbMunicipio.TabStop = true;
+            this.rbMunicipio.Text = "Municipio";
+            this.rbMunicipio.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbDepartamento);
+            this.groupBox1.Controls.Add(this.rbMunicipio);
+            this.groupBox1.Location = new System.Drawing.Point(69, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // rbDepartamento
+            // 
+            this.rbDepartamento.AutoSize = true;
+            this.rbDepartamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDepartamento.Location = new System.Drawing.Point(17, 49);
+            this.rbDepartamento.Name = "rbDepartamento";
+            this.rbDepartamento.Size = new System.Drawing.Size(142, 24);
+            this.rbDepartamento.TabIndex = 3;
+            this.rbDepartamento.TabStop = true;
+            this.rbDepartamento.Text = "Departamento";
+            this.rbDepartamento.UseVisualStyleBackColor = true;
+            // 
+            // Municipios
+            // 
+            this.Municipios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Municipios.FormattingEnabled = true;
+            this.Municipios.ItemHeight = 20;
+            this.Municipios.Location = new System.Drawing.Point(69, 194);
+            this.Municipios.Name = "Municipios";
+            this.Municipios.Size = new System.Drawing.Size(159, 124);
+            this.Municipios.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(407, 58);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(172, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // btnDEL
+            // 
+            this.btnDEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDEL.Location = new System.Drawing.Point(484, 116);
+            this.btnDEL.Name = "btnDEL";
+            this.btnDEL.Size = new System.Drawing.Size(79, 32);
+            this.btnDEL.TabIndex = 6;
+            this.btnDEL.Text = "DEL";
+            this.btnDEL.UseVisualStyleBackColor = true;
+            // 
+            // btnDerecha
+            // 
+            this.btnDerecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDerecha.Location = new System.Drawing.Point(285, 204);
+            this.btnDerecha.Name = "btnDerecha";
+            this.btnDerecha.Size = new System.Drawing.Size(79, 32);
+            this.btnDerecha.TabIndex = 7;
+            this.btnDerecha.Text = "---->";
+            this.btnDerecha.UseVisualStyleBackColor = true;
+            this.btnDerecha.Click += new System.EventHandler(this.btnDerecha_Click);
+            // 
+            // btnIzquierda
+            // 
+            this.btnIzquierda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIzquierda.Location = new System.Drawing.Point(285, 257);
+            this.btnIzquierda.Name = "btnIzquierda";
+            this.btnIzquierda.Size = new System.Drawing.Size(79, 32);
+            this.btnIzquierda.TabIndex = 8;
+            this.btnIzquierda.Text = "<---";
+            this.btnIzquierda.UseVisualStyleBackColor = true;
+            this.btnIzquierda.Click += new System.EventHandler(this.btnIzquierda_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(285, 306);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(79, 32);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Departamentos
+            // 
+            this.Departamentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Departamentos.FormattingEnabled = true;
+            this.Departamentos.ItemHeight = 20;
+            this.Departamentos.Location = new System.Drawing.Point(441, 194);
+            this.Departamentos.Name = "Departamentos";
+            this.Departamentos.Size = new System.Drawing.Size(155, 124);
+            this.Departamentos.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 430);
+            this.Controls.Add(this.Departamentos);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnIzquierda);
+            this.Controls.Add(this.btnDerecha);
+            this.Controls.Add(this.btnDEL);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.Municipios);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.v);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button v;
+        private System.Windows.Forms.RadioButton rbMunicipio;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbDepartamento;
+        private System.Windows.Forms.ListBox Municipios;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnDEL;
+        private System.Windows.Forms.Button btnDerecha;
+        private System.Windows.Forms.Button btnIzquierda;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.ListBox Departamentos;
+    }
+}
+
---
+++ Presentacion/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void v_Click(object sender, EventArgs e)
+        {
+            if (rbMunicipio.Checked == true)
+            {
+
+                Municipios.Items.Add(txtNombre.Text);
+                txtNombre.Text = "";
+
+            }
+            else if (rbDepartamento.Checked == true) { 
+            
+                Departamentos.Items.Add(txtNombre.Text);
+                txtNombre.Text = "";
+            }
+
+
+        }
+
+        private void btnDerecha_Click(object sender, EventArgs e)
+        {
+            foreach(var item in Municipios.Items)
+            {
+                Departamentos.Items.Add(item);
+            }
+        }
+
+        private void btnIzquierda_Click(object sender, EventArgs e)
+        {
+
+            foreach (var item in Departamentos.Items)
+            {
+                Municipios.Items.Add(item);
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Municipios.Items.Clear();
+            Departamentos.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
